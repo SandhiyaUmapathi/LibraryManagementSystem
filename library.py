5a00022e5d255e9d5b880c189b3d0c340453254a
SandhiyaUmapathi <sandhiya.u2023@vitstudent.ac.in>
2026-02-15 19:02:57 +0530

    Initial commit - added library module

--- library.py
+++ library.py
@@ -1,1 +1,7 @@
-print("Library Management System Started")
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book_name):
+        self.books.append(book_name)
+        print(f"{book_name} added successfully.")
